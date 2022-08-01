12776cb4536cf9d35dc17b6ac71f2551e9f435f3
master <playa8229@gmail.com>
2022-07-31 21:56:18 -0400

    My Blog Practice

---
+++ main.js
@@ -0,0 +1,12 @@
+// Devuelve el resto de la división de dos números
+// Si alguno de los dos números es `undefined`, devuelve 0
+// Tu código:
+function resto(num1, num2) {
+    num1 === undefined || num2 === undefined
+    return 0;
+
+
+}
+
+
+console.log(num1 === undefined || num2 === undefined);
